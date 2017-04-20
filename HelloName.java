934b68ffddc4f0a8f76ad6eb33982f2a0778e44d
Manar Mohammed <mkmanar762@gmail.com>
2017-04-20 10:21:43 -0700

    Add files via upload

---
+++ HelloName.java
@@ -0,0 +1,13 @@
+
+package helloname;
+
+import javax.swing.JOptionPane;
+
+public class HelloName {
+
+    public static void main(String[] args) {
+        String name =JOptionPane.showInputDialog("what is your name?");
+      System.out.println("Hello,"+name+"!");
+    }
+    
+}
